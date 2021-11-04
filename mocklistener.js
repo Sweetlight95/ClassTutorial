193f4d2fc59b15b989f7fbe454c738eb02458dfd
Sweetlight95 <princessadesuwa93@gmail.com>
2021-11-04 11:39:14 +0100

    update

---
+++ mocklistener.js
@@ -0,0 +1,23 @@
+
+let documentPseudo = {
+addEventListenerPseudo: function(event, afunction){
+    switch (event){
+        case 'click':
+            afunction(event, "1")
+            break;
+        case 'change':
+            afunction(event, "2")
+            break;
+        case 'hover':
+            afunction(event, "3")
+            break;
+        default:
+            break;
+    }
+}
+}
+
+// documentPseudo.addEventListenerPseudo("hover", (event, number) => {
+//     console.log(event, number);})
+let myFunction = (event, number) => console.log(event, number)
+documentPseudo.addEventListenerPseudo("hover", myFunction)
